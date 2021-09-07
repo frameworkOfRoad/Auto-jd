606f47a36d3454cc207d06c24952b013c4d1ae67
frameworkOfRoad <86937530+frameworkOfRoad@users.noreply.github.com>
2021-09-08 00:11:59 +0800

    Update d_jdfactory.js

--- d_jdfactory.js
+++ d_jdfactory.js
@@ -42,7 +42,7 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
-let wantProduct = ``;//心仪商品名称
+let wantProduct = `笔单支装`;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = ['T018v_V1SBYR8V3WIhmb1ACjVWnYaS5kRrbA', 'T0225KkcRh9P9FbRKUygl_UJcgCjVWnYaS5kRrbA', 'T011tvV3SBcQ8VwCjVWnYaS5kRrbA', 'T0225KkcR0pM91aBIhmgxf9bcACjVWnYaS5kRrbA', 'T0205KkcEV9ThDGWdWGw0K5uCjVWnYaS5kRrbA', 'T0225KkcRktN8lyBdEj1kaQMdwCjVWnYaS5kRrbA', 'P04z54XCjVWnYaS5uCHk7RxfanmDaDzc6FquQ'];
 !(async () => {
